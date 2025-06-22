8a52add37a2377105437be7d053c05c1ed9fb85a
Walin Paredes <paolitomaldonadofarje@gmail.com>
2025-06-21 23:49:18 -0500

    Refactor App component to include navigation for Infinity Scroll and update button accessibility in Pagination component

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,34 @@
-// import Characters from "./components/Characters"
+import { useState } from "react"
 import CharactersPagination from "./components/CharactersPagination"
 import useCharacterPagination from "./hooks/useCharacterPagination"
-// import useCharacter from "./hooks/useCharacter"
+import Characters from "./components/Characters"
+import useCharacter from "./hooks/useCharacter"
 
 function App() {
-  // const { characters, lastCharacterRef } = useCharacter()
+  const [currentPath] = useState(window.location.pathname)
+
+  const { characters: charactersInfinity, lastCharacterRef } = useCharacter()
   const { characters, totalPages, page, hasPrevius, hasNext, handleNextOrPreviusPage } = useCharacterPagination()
 
   return (
     <main>
-      {/* <Characters characters={characters} lastCharacterRef={lastCharacterRef} /> */}
 
-      <CharactersPagination characters={characters} totalPages={totalPages} page={page} hasPrevius={hasPrevius} hasNext={hasNext} onChangePage={handleNextOrPreviusPage} />
-
+      <header className="justify-center gap-6 bg-gray-800 fixed w-full z-10">
+        <nav className="p-4 flex flex-wrap justify-center gap-6 bg-gray-800 w-full">
+          <a href="/" className={`underline hover:text-amber-300 ${currentPath === '/' && 'text-amber-300'}`}>Pagination - Characters</a>
+          <a href="/infinity" className={`underline hover:text-amber-300 ${currentPath === '/infinity' && 'text-amber-300'}`}>Infinity Scroll - Characters </a>
+        </nav>
+      </header>
+      {
+        currentPath === '/infinity' && (
+          <Characters characters={charactersInfinity} lastCharacterRef={lastCharacterRef} />
+        )
+      }
+      {
+        currentPath === '/' && (
+          <CharactersPagination characters={characters} totalPages={totalPages} page={page} hasPrevius={hasPrevius} hasNext={hasNext} onChangePage={handleNextOrPreviusPage} />
+        )
+      }
     </main>
   )
 }
--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -28,12 +28,12 @@ function Pagination({ onChangePage, totalPages, page: currentPage, hasPrevius, h
               {
                 page === currentPage
                   ? (
-                    <button disabled type="button" className="flex items-center justify-center px-4 h-8  text-blue-600 border border-gray-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white">
+                    <button disabled type="button" aria-current="page" className="flex items-center justify-center px-4 h-8  text-blue-600 border border-gray-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white">
                       {page}
                     </button>
                   )
                   : (
-                    <button onClick={onChangePage({ page: page })} aria-current="page" className="flex items-center justify-center px-4 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">{page}</button>
+                    <button onClick={onChangePage({ page: page })} className="flex items-center justify-center px-4 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">{page}</button>
                   )
               }
             </li>
